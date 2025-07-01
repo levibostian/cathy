4a162a759da936bd89c02f1864f2ec3e659ee9b0
Levi Bostian <levi@levi.earth>
2025-07-01 07:42:11 -0500

    feat: overwrite comments made from previous CI runs
    Handle another use case for updating comments made over multiple CI runs.

--- app/comment.test.ts
+++ app/comment.test.ts
@@ -1,7 +1,7 @@
 import { githubAuthToken } from "./_.test"
 import { v4 as uuid } from "uuid"
 import { comment, deleteComment, getMessageHeader } from "./comment"
-import { findPreviousComment } from "./github"
+import { findPreviousComment, findPreviousComments } from "./github"
 
 const githubRepo = "levibostian/cathy"
 const githubIssue = 2
@@ -17,9 +17,40 @@ describe("comment", () => {
     })
 
     expect(actual.updatedPreviousComment).toBe(false)
-    const actualComment = await findPreviousComment(githubAuthToken, githubRepo, githubIssue, getMessageHeader("default")) 
+    const actualComments = await findPreviousComments(githubAuthToken, githubRepo, githubIssue, getMessageHeader("default"))
 
-    expect(actualComment?.body.includes(message)).toBe(true)
+    expect(actualComments.some(comment => comment.body.includes(message))).toBe(true)
+  })
+  it(`given do not update existing, expect multiple comments created`, async () => {
+    const messages = [
+      `First message, update existing false ${uuid()}`,
+      `Second message, update existing false ${uuid()}`
+    ]
+    const updateID = uuid()
+
+    const actual = await comment(messages[0], {
+      githubToken: githubAuthToken,
+      githubRepo,
+      githubIssue,
+      updateID: updateID,
+      updateExisting: false
+    })
+    expect(actual.updatedPreviousComment).toBe(false)
+
+    const actual2 = await comment(messages[1], {
+      githubToken: githubAuthToken,
+      githubRepo,
+      githubIssue,
+      updateID: updateID,
+      updateExisting: false
+    })
+    expect(actual2.updatedPreviousComment).toBe(false)
+
+    const actualComments = await findPreviousComments(githubAuthToken, githubRepo, githubIssue, getMessageHeader(updateID))
+    expect(actualComments.length).toBe(2)
+
+    expect(actualComments.some(comment => comment.body.includes(messages[0]))).toBe(true)
+    expect(actualComments.some(comment => comment.body.includes(messages[1]))).toBe(true)
   })
 
   it(`given unique comment id, expect create new comment`, async () => {
@@ -84,7 +115,6 @@ describe("comment", () => {
       githubToken: githubAuthToken,
       githubRepo,
       githubIssue,
-      updateExisting: true,
       updateID: updateId
     })
 
@@ -100,6 +130,40 @@ describe("comment", () => {
     const actualComment = await findPreviousComment(githubAuthToken, githubRepo, githubIssue, getMessageHeader(updateId))
     expect(actualComment?.body.includes(`AUTOMATED MESSAGE WITH UPDATE ID: ${updateId}`)).toBe(true)
     expect(actualComment?.body.includes(`APPENDED AUTOMATED MESSAGE WITH UPDATE ID: ${updateId}`)).toBe(true)
+  })
+
+  // Test reproducing "## Update/append over multiple CI runs" section in README. 
+  it("given append message true, given previous comment from old CI run, expect replace contents with new message", async () => {
+    const updateId1 = uuid()
+    const updateId2 = uuid()
+
+    // Let's say the CI runs on a pull request and makes this comment. 
+    await comment(`AUTOMATED MESSAGE WITH UPDATE ID: ${updateId1}`, {
+      githubToken: githubAuthToken,
+      githubRepo,
+      githubIssue,
+      appendToExisting: true,
+      updateID: ['test-coverage', `test-coverage-${updateId1}`]
+    })
+
+    // Now, let's say the CI runs again. The updateID is unique for this run. 
+    await comment(`AUTOMATED MESSAGE WITH UPDATE ID: ${updateId2}`, {
+      githubToken: githubAuthToken,
+      githubRepo,
+      githubIssue,
+      appendToExisting: true,
+      updateID: ['test-coverage', `test-coverage-${updateId2}`]
+    })
+
+    // We expect that we no longer can find the comment from the first CI run because it was replaced with the second CI run's comment.
+    expect(await findPreviousComment(githubAuthToken, githubRepo, githubIssue, getMessageHeader(`test-coverage-${updateId1}`))).toBeUndefined()
+
+    const expectComment = await findPreviousComment(githubAuthToken, githubRepo, githubIssue, getMessageHeader(`test-coverage-${updateId2}`))
+    expect(expectComment).toBeDefined()
+    expect(expectComment?.body.includes(`AUTOMATED MESSAGE WITH UPDATE ID: ${updateId2}`)).toBe(true)
+    expect(expectComment?.body.includes(getMessageHeader(`test-coverage`))).toBe(true)
+    expect(expectComment?.body.includes(getMessageHeader(`test-coverage-${updateId1}`))).toBe(false)
+    expect(expectComment?.body.includes(getMessageHeader(`test-coverage-${updateId2}`))).toBe(true)
   })
 })
 
--- app/comment.ts
+++ app/comment.ts
@@ -14,23 +14,47 @@ export const comment = async (message: string, options: SpeakOptions): Promise<C
   if (!options.updateID) options.updateID = "default"  
   if (!options.updateExisting) options.updateExisting = false 
   if (!options.appendToExisting) options.appendToExisting = false
+  const updateId = Array.isArray(options.updateID) ? options.updateID : [options.updateID]
+  if (options.appendToExisting === true) options.updateExisting = true // If we are appending to an existing comment, we must update the existing comment
 
-  const messageHeader = getMessageHeader(options.updateID)
-  message = `${messageHeader}\n${message}`
+  /**
+   * Find an existing comment, if it exists. 
+   */
   let githubComment: IssueComment | undefined
+  let doesExistingCommentContainAllUpdateIds = false 
 
-  if (options.updateExisting || options.appendToExisting) {
-    githubComment = await findPreviousComment(
-      options.githubToken,
-      options.githubRepo,
-      options.githubIssue,
-      messageHeader
-    )
+  // Only search for existing if updateExisting is true so we keep variables above in a false state.
+  if (options.updateExisting) {
+    for await (const _updateID of updateId) {
+      const updateID = _updateID.trim()
+      if (!updateID || updateID == "") continue // Skip empty update IDs
+      if (githubComment) break // If we already found a comment, no need to search again
+
+      const messageHeader = getMessageHeader(updateID)
+
+      // Search for a comment that contains at least one of the message headers. 
+      githubComment = await findPreviousComment(
+        options.githubToken,
+        options.githubRepo,
+        options.githubIssue,
+        messageHeader
+      )
+
+      // If we found a comment, check if it contains all update IDs
+      doesExistingCommentContainAllUpdateIds = updateId.every(updateID => githubComment?.body.includes(getMessageHeader(updateID))) ?? false;
+    };
   }
 
-  if (options.appendToExisting && githubComment) {
+  const willAppendToExisting = options.appendToExisting && doesExistingCommentContainAllUpdateIds
+  if (willAppendToExisting && githubComment) {
     message = `${githubComment.body}\n\n${message}`
   }
+
+  // Prepend the message header to the message, for all of the update IDs 
+  updateId.forEach((_updateID) => {
+    const messageHeader = getMessageHeader(_updateID)
+    message = `${messageHeader}\n${message}`
+  })
 
   await makeComment(
     options.githubToken,
